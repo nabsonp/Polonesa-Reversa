f680710aa20aa4fe4a6c994a143013307d089de7
Nabson Paiva <nabsonp@gmail.com>
2018-06-07 21:36:02 -0400

    Funciona todas as operações se forem únicas

---
+++ q02.py
@@ -0,0 +1,30 @@
+pos = input()
+pilha = []
+for i in range(len(pos)):
+    if pos[i] == '*':
+        x = int(pilha[len(pilha)-1])
+        y = int(pilha[len(pilha)-2])
+        del pilha[len(pilha)-1]
+        del pilha[len(pilha)-2]
+        pilha.append(x * y)
+    elif pos[i] == '/':
+        x = int(pilha[len(pilha)-1])
+        y = int(pilha[len(pilha)-2])
+        del pilha[len(pilha)-1]
+        del pilha[len(pilha)-2]
+        pilha.append(y / x)
+    elif pos[i] == '-':
+        x = int(pilha[len(pilha)-1])
+        y = int(pilha[len(pilha)-2])
+        del pilha[len(pilha)-1]
+        del pilha[len(pilha)-2]
+        pilha.append(y - x)
+    elif pos[i] == '+':
+        x = int(pilha[len(pilha)-1])
+        y = int(pilha[len(pilha)-2])
+        del pilha[len(pilha)-1]
+        del pilha[len(pilha)-2]
+        pilha.append(x + y)
+    else:
+        pilha.append(pos[i])
+print(pilha[0])

6f1ec1e1726b0e7350a8ddd6321d2eb9d31118f2
Nabson Paiva <nabsonp@gmail.com>
2018-06-07 22:50:05 -0400

    Funcinoando sem parêntesis

--- q01.py
+++ q01.py
@@ -1,22 +1,26 @@
 oper = input().split()
 for i in range(len(oper)-1):
-	print("   ",i,"   ")
 	if (oper[i] == '+' or oper[i] == '-') and (oper[i+1] != '+' and
-	 oper[i+1] != '-' and oper[i+1] != '*' and oper[i+1] != '/' and oper[i+1] != ')' and oper[i+1] != '('):
+	 oper[i+1] != '-' and oper[i+1] != '*' and oper[i+1] != '/' and
+	 oper[i+1] != ')' and oper[i+1] != '('):
 		aux = oper[i+1]
 		oper[i+1] = oper[i]
 		oper[i] = aux
-		i = i + 1
-	elif (oper[i] == '*' or oper[i] == '/') and (oper[i+1] != '+' and oper[i+1] != '-' and oper[i+1] != '*' and oper[i+1] != '/' ):
+	elif (oper[i] == '*' or oper[i] == '/') and (oper[i+1] != '+' and
+	oper[i+1] != '-' and oper[i+1] != '*' and oper[i+1] != '/' ):
 		if oper[i-1] == '+' or oper[i-1] == '-':
 			aux = oper[i-1]
 			oper[i-1] = oper[i+1]
 			oper[i+1] = aux
 		elif oper[i-1] == ')':
 			oper[i-1] = ''
-		aux = oper[i+1]
-		oper[i+1] = oper[i]
-		oper[i] = aux
+			aux = oper[i+1]
+			oper[i+1] = oper[i]
+			oper[i] = aux
+		else:
+			aux = oper[i+1]
+			oper[i+1] = oper[i]
+			oper[i] = aux
 	elif oper[i] == '(':
 		oper[i] = ''
 	for j in range(len(oper)):

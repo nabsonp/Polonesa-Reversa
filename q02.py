058106a7b573e595888e88a4beedb69e188b0f1f
Nabson Paiva <nabsonp@gmail.com>
2018-06-07 21:48:13 -0400

    Agora funfa pra eq. maiores. Resta testar

--- q02.py
+++ q02.py
@@ -5,25 +5,25 @@ for i in range(len(pos)):
         x = int(pilha[len(pilha)-1])
         y = int(pilha[len(pilha)-2])
         del pilha[len(pilha)-1]
-        del pilha[len(pilha)-2]
+        del pilha[len(pilha)-1]
         pilha.append(x * y)
     elif pos[i] == '/':
         x = int(pilha[len(pilha)-1])
         y = int(pilha[len(pilha)-2])
         del pilha[len(pilha)-1]
-        del pilha[len(pilha)-2]
+        del pilha[len(pilha)-1]
         pilha.append(y / x)
     elif pos[i] == '-':
         x = int(pilha[len(pilha)-1])
         y = int(pilha[len(pilha)-2])
         del pilha[len(pilha)-1]
-        del pilha[len(pilha)-2]
+        del pilha[len(pilha)-1]
         pilha.append(y - x)
     elif pos[i] == '+':
         x = int(pilha[len(pilha)-1])
         y = int(pilha[len(pilha)-2])
         del pilha[len(pilha)-1]
-        del pilha[len(pilha)-2]
+        del pilha[len(pilha)-1]
         pilha.append(x + y)
     else:
         pilha.append(pos[i])
